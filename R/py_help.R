998267779509441256454080cfef03d7af51f6d6
skuethe <crazycapivara@gmail.com>
2017-01-21 20:03:43 +0100

    Add possibilty to put python help to stdout

--- R/py_help.R
+++ R/py_help.R
@@ -1,4 +1,4 @@
-py_help <- function(func_name){
+py_help <- function(func_name, stdout = ""){
   py_cmd <- sprintf('"print(%s.__doc__)"', func_name)
-  system2(get_python_cmd(), c("-c", py_cmd))
+  system2(get_python_cmd(), c("-c", py_cmd), stdout = stdout)
 }
